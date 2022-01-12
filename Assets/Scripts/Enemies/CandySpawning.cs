ff35f6067d1656ecb16868fc343a03ea670a1626
Athanasios Nektarios Karachalios Stagkas <41343849+Pyrofanis@users.noreply.github.com>
2022-01-12 16:20:09 +0200

    ProgressM8

--- Assets/Scripts/Enemies/CandySpawning.cs
+++ Assets/Scripts/Enemies/CandySpawning.cs
@@ -7,7 +7,7 @@ public class CandySpawning : MonoBehaviour
     [Header("Points that candies will spawn from")]
     [Header("ListOfPoints")]
     [SerializeField]
-    private GameObject[] spawnPoints; 
+    private GameObject[] spawnPoints;
 
     [SerializeField]
     [Header("Candies to spawn From")]
@@ -15,7 +15,7 @@ public class CandySpawning : MonoBehaviour
     private GameObject[] candyPrefabs;
 
     private List<GameObject> activeCandies;
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,10 +27,21 @@ public class CandySpawning : MonoBehaviour
     {
 
     }
-
-    private void OnDrawGizmos()
+    private GameObject GetPrefab(GameObject[] candyPrefabs)
     {
-    
+        int index = Random.Range(0, candyPrefabs.Length);
+        return candyPrefabs[index];
+    }
+    private Vector3 GetLocation(GameObject[] spawnPoints)
+    {
+        int index = Random.Range(0, spawnPoints.Length);
+        return spawnPoints[index].transform.position;
+    }
+
+    private void InstantiateObjects()
+    {
+        GameObject candy = Instantiate(GetPrefab(candyPrefabs), GetLocation(spawnPoints), Quaternion.identity);
+        activeCandies.Add(candy);
     }
     private GameObject ObjectToInstantiate()
 
@@ -45,12 +56,6 @@ public class CandySpawning : MonoBehaviour
             if (!activeCandies.Contains(currentCandy))
                 activeCandies.Add(currentCandy);
         }
-    }
-    private Vector2 RandomPosition(Vector3 leftPos, Vector3 rightPos)
-    {
-
-        float randomX = Random.Range(leftPos.x, rightPos.x);
-        return new Vector2(randomX, leftPos.y);
     }
 }
 
